1174d100db982bfb2e251edbbc27584ab1b86a6a
NoahHig <noahhigpen@gmail.com>
2026-05-24 17:40:50 -0500

    Implemented express.js and created initial a-frame

---
+++ server.js
@@ -0,0 +1,15 @@
+console.log("Howdy World");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.listen(port);
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.send("Howdy World");
+});
+
+app.get("/poker", (req, res) => {
+  res.render("index");
+});
